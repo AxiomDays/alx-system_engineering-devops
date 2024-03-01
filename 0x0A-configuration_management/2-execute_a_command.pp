349d5e8bb61cc607d108923a5ebce4d81723b4ce
AxiomDays <tobeoke@gmail.com>
2024-03-01 22:12:53 +0100

    task2.1

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 #Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'kill' :
-	command => 'pkill killmenow',
-	path => '/bin/'
+  command => 'pkill killmenow',
+  path    => '/bin/'
 }
