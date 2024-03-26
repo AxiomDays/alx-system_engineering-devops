a1f76f5fc654f3af47a2fe17edb2c7c92fdde5eb
AxiomDays <tobeoke@gmail.com>
2024-03-26 02:11:55 +0100

    task0.1

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress."""
+"""
+Write a Python script that, using this REST API,
+for a given employee ID, returns information about his/her TODO list progress.
+"""
 if __name__ == "__main__":
     import requests
     import sys
@@ -7,7 +10,8 @@ if __name__ == "__main__":
     url = "https://jsonplaceholder.typicode.com/"
     x = requests.get("{}users/{}".format(url, sys.argv[1])).json()
     y = requests.get("{}todos?userId={}".format(url, sys.argv[1])).json()
-    z =  requests.get("{}todos?userId={}&completed=true".format(url, sys.argv[1])).json()
+    z = requests.get(
+            "{}todos?userId={}&completed=true".format(url, sys.argv[1])).json()
 
     print(f"Employee {x.get('name')} is done with tasks({len(z)}/{len(y)}):")
     for tasks in z:
