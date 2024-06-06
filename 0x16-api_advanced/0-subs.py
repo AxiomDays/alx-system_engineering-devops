7ea66700717b4df600fd1de49ad5f603f43cc1bd
AxiomDays <tobeoke@gmail.com>
2024-06-06 03:50:04 +0100

    task0.11111

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,7 +12,7 @@ def number_of_subscribers(subreddit):
     res = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={"User-Agent": "Custom"},)
 
     if res.status_code == 200:
-        return int(res.json().get("data").get("subscribers"))
+        return res.json().get("data").get("subscribers")
     else:
         return 0
 
