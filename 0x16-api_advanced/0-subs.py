5db75352ab911ecdd7ffecf92babaf6ccc8bb955
AxiomDays <tobeoke@gmail.com>
2024-06-06 03:54:20 +0100

    task0.2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,12 +3,12 @@
 0-subs
 script to query the API and return the number of subscribers
 """
+import requests
+
 def number_of_subscribers(subreddit):
     """
     the function in question
     """
-    import requests
-        
     res = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={"User-Agent": "Custom"},)
 
     if res.status_code == 200:
